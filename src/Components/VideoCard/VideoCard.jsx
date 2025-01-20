e552a61f07d8ae45a270920c2f32038231972b60
fesao09 <feoliveira339@gmail.com>
2025-01-20 17:01:06 -0300

    Add files via upload

--- src/Components/VideoCard/VideoCard.jsx
+++ src/Components/VideoCard/VideoCard.jsx
@@ -1,1 +1,34 @@
+import React from "react";
+import {
+  FrontEndTitle,
+  BackEndTitle,
+  FrontEndCategoryTitle,
+  BackEndCategoryTitle,
+  MobileCategoryTitle,
+} from "../../Pages/NewVideo/NewVideo.styled";
 
+const VideoCard = ({ video }) => {
+  return (
+    <div>
+      {video.categoria === "Front End" ? (
+        <FrontEndTitle>{video.titulo}</FrontEndTitle>
+      ) : video.categoria === "Back End" ? (
+        <BackEndTitle>{video.titulo}</BackEndTitle>
+      ) : (
+        <h1>{video.titulo}</h1>
+      )}
+      {video.categoria === "Front End" ? (
+        <FrontEndCategoryTitle>{video.categoria}</FrontEndCategoryTitle>
+      ) : video.categoria === "Back End" ? (
+        <BackEndCategoryTitle>{video.categoria}</BackEndCategoryTitle>
+      ) : video.categoria === "Mobile" ? (
+        <MobileCategoryTitle>{video.categoria}</MobileCategoryTitle>
+      ) : (
+        <h2>{video.categoria}</h2>
+      )}
+      {/* ...existing code... */}
+    </div>
+  );
+};
+
+export default VideoCard;
